b5c1c1f7b1ca8d036a0fdfb547e4849c69896be1
priscila espinoza <pr.espinoza@duocuc.cl>
2022-09-08 10:26:46 -0300

    ejercicio2

---
+++ hola_mundo/hola_mundo.py
@@ -0,0 +1,18 @@
+# 1. TAREA: imprime "Hola, mundo"
+print( "hola mundo")
+
+# 2. imprime "Hola, Noelle" con el nombre en una variable
+name = "Noelle"
+print( "hola", name )	# con una coma
+
+print( "hola"+name )	# con un +
+# 3. imprimir "Hola 42!" con el número en una variable
+name = 42
+"""print( tu código aquí )	# con una coma
+print( tu código aquí )	# con una +	-- este debería arrojar un error!
+# 4. imprimir "Amo comer sushi y pizza" con las comidas en variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( tu código aquí ) # con .format()
+print( tu código aquí ) # con una cadena f"""
+
