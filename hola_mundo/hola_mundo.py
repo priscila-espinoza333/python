1a791ddc6b44ee1bfbff14e96228eb7ba8de3115
priscila espinoza <pr.espinoza@duocuc.cl>
2022-09-12 01:43:45 -0300

    ejercicio4

--- hola_mundo/hola_mundo.py
+++ hola_mundo/hola_mundo.py
@@ -7,15 +7,13 @@ print( "hola", name )	# con una coma
 print( "hola"+name )	# con un +
 
 # 3. imprimir "Hola 42!" con el número en una variable
-name = 42
 var = 42
-print("hola", var)
+print("hola", var)# con una coma
+#print("hola"+var )	# con una +	-- este debería arrojar un error!
 
-"""print( tu código aquí )	# con una coma
-print( tu código aquí )	# con una +	-- este debería arrojar un error!
 # 4. imprimir "Amo comer sushi y pizza" con las comidas en variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
-print( tu código aquí ) # con .format()
-print( tu código aquí ) # con una cadena f"""
+print("Amo comer{}".format(fave_food2)) # con .format()
+print(f"Amo comer", fave_food1 ) # con una cadena f"""
 
